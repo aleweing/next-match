0c7d34469a5bf4099f0265adb35987dcfcde06b5
aleweing <alewein@proton.me>
2026-06-12 13:14:55 +0200

    Update app.js
    corrección countdown

--- js/app.js
+++ js/app.js
@@ -339,18 +339,6 @@ getCountdown(dateStr, timeStr) {
     }
 }
         
-        const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-
-        if (days > 0) {
-            return `En ${days}d ${hours}h`;
-        } else if (hours > 0) {
-            return `En ${hours}h ${minutes}m`;
-        } else {
-            return `En ${minutes}m`;
-        }
-    }
 
     updatePositionIndicator() {
         document.getElementById("currentPosition").textContent = this.currentMatchIndex + 1;
